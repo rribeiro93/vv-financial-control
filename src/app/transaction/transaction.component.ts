e09555aff270af12abdc217c6c2d3817a1d7ded5
Rafael Ribeiro <rribeiro93k@gmail.com>
2019-02-16 16:50:12 -0200

    Creating transactions components

---
+++ src/app/transaction/transaction.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vv-transaction',
+  templateUrl: './transaction.component.html',
+  styleUrls: ['./transaction.component.scss']
+})
+export class TransactionComponent {
+
+  constructor() { }
+
+}
---
+++ src/app/transactions-statement/transactions-statement.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vv-transactions-statement',
+  templateUrl: './transactions-statement.component.html',
+  styleUrls: ['./transactions-statement.component.scss']
+})
+export class TransactionsStatementComponent {
+
+  constructor() { }
+
+}
